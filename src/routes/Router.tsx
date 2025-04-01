cca6b11b5c315acb2a228c5ffebd8702fc179111
손성현 <sunghyun7895@naver.com>
2025-04-01 14:55:14 +0900

    개인 공부 정리 및 Shadow.tsx 에러 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,6 +5,14 @@
 import { lazy } from 'react';
 import { Navigate, createBrowserRouter } from 'react-router';
 
+// lazy() 사용이유는 필요할 떄만 해당 컴포넌트를 로딩해서 앱 성능을 높이기 위함에 있다.(코드 스플리팅)
+// 즉, 반대로 생각하면 리액트는 lazy()를 사용하지 않는 경우 정적 import를 통해 모든 컴포넌트를 번들에 한꺼번에 포함시킨다.
+// 예를 들어, 
+// import Dashboard from './Dashboard';
+// import Profile from './Profile';
+// import NotiList from './NotiList';
+// 정적 import를 할 경우 Profile 페이지를 방문하지 않았음에도 이미 로딩되어 있다.
+
 /* ***Layouts**** */
 const FullLayout = lazy(() => import('../layouts/full/FullLayout'));
 const BlankLayout = lazy(() => import('../layouts/blank/BlankLayout'));
